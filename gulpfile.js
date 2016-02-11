211bef2303b49084338708bbb86bb83a9290caa2
Hiromune Ito <muniere.fujiwara@gmail.com>
2016-02-12 00:26:13 +0900

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var del = require('del');
+var mkdirp = require('mkdirp');
+var runSequence = require('run-sequence');
+var wiredep = require('wiredep').stream;
+
+var $ = gulpLoadPlugins();
+
+gulp.task('extras', function() {
+  return gulp.src([
+    'app/*.*',
+    'app/_locales/**',
+    '!app/*.json',
+    '!app/*.html',
+  ], {
+    base: 'app',
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['app/scripts/**/*.js'])
+    .pipe($.eslint({ env: { es6: false } }))
+    .pipe($.eslint.format());
+});
+
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*')
+    .pipe($.if($.if.isFile, $.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    }))
+    .on('error', function (err) {
+      console.log(err);
+      this.end();
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('html',  function() {
+  var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
+
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe($.sourcemaps.init())
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.minifyCss({compatibility: '*'})))
+    .pipe($.sourcemaps.write())
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('chromeManifest', function() {
+  return gulp.src('app/manifest.json')
+    .pipe($.chromeManifest({
+      buildnumber: null,
+      background: {
+        target: 'scripts/background.js',
+        exclude: []
+      }
+  }))
+  .pipe($.if('*.css', $.minifyCss({compatibility: '*'})))
+  .pipe($.if('*.js', $.sourcemaps.init()))
+  .pipe($.if('*.js', $.uglify()))
+  .pipe($.if('*.js', $.sourcemaps.write('.')))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('watch', ['lint', 'html'], function() {
+  $.livereload.listen();
+
+  gulp.watch([
+    'app/*.html',
+    'app/libraries/**/*',
+    'app/scripts/**/*.js',
+    'app/images/**/*',
+    'app/styles/**/*',
+    'app/_locales/**/*.json'
+  ]).on('change', $.livereload.reload);
+
+  gulp.watch('app/scripts/**/*.js', ['lint']);
+  gulp.watch('bower.json', ['wiredep']);
+});
+
+gulp.task('size', function() {
+  return gulp.src('dist/**/*').pipe($.size({ title: 'build', gzip: true }));
+});
+
+gulp.task('wiredep', function() {
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('build', function(cb) {
+  runSequence(
+    'lint', 'chromeManifest',
+    ['html', 'images', 'extras'],
+    'size', cb);
+});
+
+gulp.task('package', ['build'], function() {
+  var ChromeExtension = require('crx');
+  var manifest = require('./dist/manifest.json');
+
+  var crx = new ChromeExtension({
+    rootDirectory: 'dist',
+    privateKey: fs.readFileSync(path.join(__dirname, 'wiki-style.pem'))
+  });
+
+  crx.load()
+    .then(function(crx) {
+      return crx.loadContents();
+    })
+    .then(function(buffer) {
+      return crx.pack(buffer);
+    })
+    .then(function(buffer) {
+      fs.writeFileSync('crx/wiki-style-' + manifest.version + '.crx', buffer);
+    });
+});
+
+gulp.task('default', ['clean'], function(cb) {
+  runSequence('build', cb);
+});
