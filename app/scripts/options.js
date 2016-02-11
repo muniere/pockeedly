b656dce438ea38a216fa910ef72f4b6e82364fe4
Hiromune Ito <muniere.fujiwara@gmail.com>
2016-02-12 03:03:03 +0900

    Layout options.html

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,37 @@
-'use strict';
+(function() {
+  'use strict';
 
-console.log('\'Allo \'Allo! Option');
+  /* global angular */
+
+  angular.module('scm', [])
+    .controller('RootController', function($scope) {
+      $scope.shortcuts = [
+        {
+          key: 'ctrl+c',
+          name: 'ccopycopycopycopycopycopycopyopy',
+          script: 'console.log("copy");',
+          patterns: ''
+        },
+        {
+          key: 'ctrl+v',
+          name: 'paste',
+          script: 'console.log("paste");',
+          patterns: ''
+        }
+      ];
+
+      $scope.shortcut = $scope.shortcuts[0];
+
+      $scope.activate = function(index) {
+        $scope.shortcut = $scope.shortcuts[index];
+      };
+
+      $scope.save = function(index) {
+        console.log('Save pref at index ' + index);
+      };
+
+      $scope.remove = function(index) {
+        console.log('Remove pref at index ' + index);
+      };
+    });
+})();
